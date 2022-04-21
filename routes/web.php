2533d570af5c38b5f774c0eee6bf5b1e4a050b65
Your Name <you@example.com>
2022-04-21 17:34:34 -0500

    crear proyecto y arreglos

---
+++ routes/web.php
@@ -0,0 +1,124 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Primera ruta
+
+Route::get('hola' , function(){ 
+
+echo "Tengo sueño, Tokyo Revengers el besto shonen"; 
+
+});
+
+//Ruta de arreglos: 
+
+Route::get('arreglo' , function(){
+
+    $estudiantes = [ 
+
+    "CA" => 1, 
+    "JO" => true, 
+    "AN" => 2.45 
+ 
+    ];
+
+//Recorrer el arreglo 
+
+foreach($estudiantes as $e){
+
+    echo $e."<hr />"; 
+
+}
+
+// Areglos elementos en PHP 
+
+$estudiantes[] = "Camilo"; 
+
+echo "<pre>"; 
+var_dump($estudiantes); 
+echo "</pre>";  
+
+}); 
+
+
+
+Route::get('paises' , function(){
+
+    $paises = [
+
+    "Colombia" => [
+
+        "capital" => "Bogotá", 
+        "moneda" => "Peso", 
+        "población" => 51,
+        "ciudades" => [
+
+            "Medellin", 
+            "Cali", 
+            "Barranquilla"
+        ]
+     ],
+
+    "Perú" => [ 
+
+        "capital" => "Lima",
+        "moneda" => "Sol",
+        "población" => 32, 
+        "ciudades" => [
+
+            "Arequipa", 
+            "Trujillo"
+        ]
+    ], 
+
+    "Paraguay" => [ 
+
+        "capital" => "Asunción",
+        "moneda" => "Guaraní",
+        "población" => 7,
+        "ciudades" => [
+
+            "Luque"
+        ]
+    ],
+
+    "Argentina" => [
+
+        "capital" => "Buenos Aires", 
+        "moneda" => "Peso Argentino", 
+        "población" => 45, 
+    ],
+
+    "Ecuador" => [
+
+        "capital" => "Quito", 
+        "moneda" => "Dólar", 
+        "población" => 17
+
+
+    ]
+    ]; 
+
+// Mostrar la vista 
+
+return view('paises')
+->with("paises", $paises); 
+
+}); 
+
+
